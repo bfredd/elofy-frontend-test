b8d1af99df798dbe571f4b53e24b551fef9caca2
Bruno Trindade <brunofredericot@gmail.com>
2023-09-05 16:12:44 -0300

    Finish

---
+++ README.md
@@ -0,0 +1,12 @@
+## Configure and run project
+yarn dev
+
+## About project
+
+Test is on index page
+Script writed into src/components/HelloWorld.vue
+
+For the testing you need change value of word constant and refresh page.
+You can see the result on the console browser
+
+Thanks!
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <div class="text-body-2 font-weight-light mb-n1">Write Word</div>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+
+  const word = 'Reviver';
+
+  testIsPolindromo(word);
+
+  function testIsPolindromo(word) {
+
+      var result = true;
+      
+      const chars = word.length;
+      for(var i=0;i<chars;i++){
+        if(!charIsEqualInPosition(word, word[i], i)){
+          result = false;
+        }
+      }
+
+      const message = result ? 'It\'s OK' : 'Failed!';
+      console.log(message);
+
+  }
+
+  function charIsEqualInPosition(word, char, position){
+
+    char    = char.toLowerCase();
+    word  = word.toLowerCase();
+
+    // invert string
+    var newWord = '';
+    const chars = word.length;
+    
+    for(var i=(chars-1);i>=0;i--){
+      newWord += word[i];
+    }
+
+    return (newWord[position]==char);
+
+  }
+
+</script>
+
+<style scoped>
+  .input{
+    margin-top: 10px;
+    border: 1px solid #ddd;
+  }
+</style>
